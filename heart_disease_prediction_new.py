62939f7c8d1555b06c6b1d998a80478be010c90d
Tushar Kumar <117516567+Tshar-k@users.noreply.github.com>
2023-02-16 18:35:28 +0530

    New Uploads

---
+++ heart_disease_prediction_new.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 16 17:28:39 2023
+
+@author: HP
+"""
+
+import numpy as np 
+import pickle 
+import streamlit as st
+
+loaded_model = pickle.load(open("E:\ML project\SVM\model pickel","rb"))
+
+def heart_disease_prediction(input_data):
+    input_data_as_np_array = np.asarray(input_data)
+    
+    input_data_reshape = input_data_as_np_array.reshape(1,-1)
+    
+    prediction = loaded_model.predict(input_data_reshape)
+    print(prediction)
+    
+    if(prediction[0] == 0):
+        return "The person does not have heart disease"
+    else:
+        return "The person have heart disease"
+
+def main():
+    st.title("Heart Disease Prediction Web App")
+    
+    # getting input from the user
+    
+    Age = st.text_input('Age')
+    Sex = st.text_input('Sex (1->Male, 0->Female)')
+    Cp = st.text_input('Chest Pain Type(0,1,2,3)')
+    trestbps = st.text_input('Resting Blood Pressure (in mm Hg on admission to the hospital)')
+    chol = st.text_input('Serum Cholestral in mg/dl')
+    fbs = st.text_input('Fasting Blood Sugar (1->True, 0->False)')
+    restecg = st.text_input('Resting Electrocardiographic Results')
+    thalach = st.text_input('Maximum Heart Rate achieved')
+    exang = st.text_input('Exercise Induced Angina (1 = yes; 0 = no)')
+    oldpeak = st.text_input('ST depression induced by exercise relative to rest')
+    slope = st.text_input('The Slope of the peak exercise ST segment')
+    ca = st.text_input('Number of major vessels (0-3) colored by flourosopy')
+    thal = st.text_input('1 = Normal; 2 = Fixed defect; 3 = Reversable defect')
+    
+  # code for prediction 
+    diagnosis = ""
+    
+    if st.button("Test Results : "):
+        diagnosis = heart_disease_prediction([Age,Sex,Cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal])
+    
+    st.success(diagnosis)
+
+if __name__ == "__main__":
+    main()
+    
